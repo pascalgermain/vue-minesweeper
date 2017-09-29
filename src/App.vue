60605445b9a4016425078249ae32634f487de13b
Pascal Germain <pascalgermain@me.com>
2017-09-29 14:26:45 +0200

    Rename Hello component to Minesweeper

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <minesweeper />
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Minesweeper from './components/Minesweeper'
 
 export default {
-  name: 'app',
   components: {
-    Hello
+    Minesweeper
   }
 }
 </script>
---
+++ src/components/Minesweeper.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="minesweeper">
+    <h1>{{ title }}</h1>
+    <div>
+      <h2></h2>
+      <div></div>
+      <h2></h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Minesweeper'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+a {
+  color: #42b983;
+}
+</style>
