528142728445c4334347413f2903ca00999c73e2
Pascal Germain <pascalgermain@me.com>
2017-09-29 14:49:19 +0200

    Add basic UI

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,11 @@ export default {
 
 <style>
 #app {
+  margin: 50px 0;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  text-align: center;
 }
 </style>
--- src/components/Minesweeper.vue
+++ src/components/Minesweeper.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="minesweeper">
     <h1>{{ title }}</h1>
-    <div>
-      <h2></h2>
-      <div></div>
-      <h2></h2>
+    <div class="game">
+      <div class="message">{{ message }}</div>
+      <div class="board"></div>
+      <button class="action">{{ action }}</button>
     </div>
   </div>
 </template>
@@ -13,18 +13,45 @@
 export default {
   data () {
     return {
-      title: 'Minesweeper'
+      title: 'Minesweeper',
+      message: '',
+      action: 'Start'
     }
   }
 }
 </script>
 
 <style scoped>
-h1, h2 {
+h1 {
   font-weight: normal;
 }
 
-a {
-  color: #42b983;
+.game {
+  display: inline-block;
+  padding: 3px;
+  background: #ccc;
+}
+
+.board {
+  width: 500px;
+  height: 500px;
+  margin: 3px 0;
+  background: #ddd;
+}
+
+.message,
+.action {
+  height: 30px;
+  background: #42b983;
+  font-size: 22px;
+  color: #fff;
+  text-transform: uppercase;
+}
+
+.action {
+  width: 100%;
+  border: 0;
+  outline: 0;
+  cursor: pointer;
 }
 </style>
